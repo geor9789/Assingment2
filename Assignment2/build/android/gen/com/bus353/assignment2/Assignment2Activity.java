f44b29ad12b527a25460ad5d33333fb043a18b67
geor9789 <geor9789@vandals.uidaho.edu>
2015-02-24 00:17:03 -0800

    Doing the assingment

---
+++ Assignment2/build/android/gen/com/bus353/assignment2/Assignment2Activity.java
@@ -0,0 +1,7 @@
+package com.bus353.assignment2;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+public final class Assignment2Activity extends TiRootActivity
+{
+}
---
+++ Assignment2/build/android/gen/com/bus353/assignment2/Assignment2AppInfo.java
@@ -0,0 +1,70 @@
+package com.bus353.assignment2;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class Assignment2AppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public Assignment2AppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.bus353.assignment2";
+	}
+
+	public String getName() {
+		return "Assignment2";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "Anthony";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by Anthony";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "900c5e1a-fbc8-405e-a6cd-1a42aa788e74";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
